c9ad26eabaae41d035c14414d6436b132813629b
Lijianpen <57410pengge>
2020-02-07 14:18:12 +0800

    项目起步- Element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import route from "./route/index"
+import ele from "element-ui"
+
+
+// 注册
+Vue.use(ele)
 Vue.config.productionTip = false
 
 new Vue({
+  route,
   render: h => h(App),
 }).$mount('#app')
