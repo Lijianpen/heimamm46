701fb9ab6687e14daf73389acbf6586370c69951
Lijianpen <57410pengge>
2020-02-08 21:37:48 +0800

    登录模块静态页面完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,60 +1,126 @@
 <template>
+  <!-- 整体容器 -->
   <div class="login-container">
+    <!-- 左侧盒子 -->
     <div class="left-box">
-      <div class="ding">
-        <img src="../../assets/imgs/矢量智能对象 拷贝 9.png" alt />
-        <p>黑马面面</p>
-        <i></i>
-        <span>用户登录</span>
+      <!-- 顶部的标题 -->
+      <div class="title-box">
+        <img src="../../assets/imgs//矢量智能对象 拷贝 9.png" alt class="logo" />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
       </div>
-      <div class="zhong">
-        <el-input
-          class="MobileLei"
-          prefix-icon="el-icon-user"
-          placeholder="请输入手机号"
-          v-model="Mobile"
-        ></el-input>
-        <el-input
-          class="cipher"
-          prefix-icon="el-icon-lock"
-          placeholder="请输入密码"
-          v-model="cipher"
-          show-password
-        ></el-input>
-        <el-input class="code" placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="code"></el-input>
-        <el-row>
-          <el-col class="item" :span="7"></el-col>
-        </el-row>
-      </div>
-      <div class="xieyi">
-        <el-checkbox class="hezi" v-model="checked"></el-checkbox>
-        <p>我已阅读并同意</p>
-        <el-link type="primary" class="da">用户协议</el-link>
-        <p>和</p>
-        <el-link type="primary" class="da">隐私条款</el-link>
-      </div>
-      <div class="buttons">
-        <el-button class="button_an" type="primary">登录</el-button>
-        <el-button class="button_an" type="primary">注册</el-button>
-      </div>
+      <!-- 表单 -->
+      <el-form ref="loginForm" :rules="rules" :model="loginForm" label-width="43px">
+        <!-- 手机号 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号" v-model="loginForm.phone"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item prop="loginCode">
+          <el-row>
+            <el-col :span="17">
+              <el-input
+                prefix-icon="el-icon-key"
+                placeholder="请输入验证码"
+                v-model="loginForm.loginCode"
+              ></el-input>
+            </el-col>
+            <el-col :span="7" class="code-col">
+              <!-- 登录验证码 -->
+              <img class="login-code" src="../../assets/imgs/微信图片_20200208112012.png" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <!-- 用户协议 -->
+        <el-form-item>
+          <el-checkbox v-model="loginForm.isChecked">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="my-btn" @click="submitForm('loginForm')" type="primary">登录</el-button>
+          <el-button @click="showRegister" class="my-btn" type="primary">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+    <!-- 右边的图片 -->
     <img src="../../assets/imgs/login_banner_ele.png" alt />
+    <registerDialog ref="registerDialog"></registerDialog>
   </div>
+  <!-- <registerDialog></registerDialog> -->
 </template>
+
 <script>
+// 导入 注册对话框组件
+import registerDialog from "./coponents/registerDialog.vue";
 export default {
-  nama: "login",
-  //名字
+  // 组件的名字
+  name: "login",
+  // 注册组件
+  components: {
+    registerDialog // 省略了 属性值
+  },
   data() {
     return {
-      Mobile: "",
-      cipher: "",
-      code: "",
-      checked: ""
+      loginForm: {
+        // 手机号
+        phone: "",
+        // 密码
+        password: "",
+        // 验证码
+        loginCode: "",
+        // 是否勾选
+        isChecked: false
+      },
+      // 校验规则
+      rules: {
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 12, message: "密码的长度为6-12位", trigger: "blur" }
+        ],
+        loginCode: [
+          { required: true, message: "验证码不能为空", trigger: "blur" },
+          { min: 4, max: 4, message: "验证码的长度为4位", trigger: "blur" }
+        ]
+      }
     };
+  },
+  // 方法
+  methods: {
+    // 提交表单
+    submitForm(formName) {
+      // 等同于 this.$refs['loginForm'] 相当于获取到了Element-ui的表单
+      // this.$refs['loginForm'] 等同于 this.$refs.loginForm
+      // validate这个方法是Element-ui的表单的方法
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$message.success("验证成功");
+          // 验证正确
+        } else {
+          this.$message.error("验证失败");
+          // 验证错误
+          return false;
+        }
+      });
+    },
+    // 显示注册对话框
+    showRegister() {
+      // this.$refs 可以获取所有设置了ref属性的元素，包括组件
+      // registerDialog 和上面设置的属性要一致
+      // 也可以用 this.$refs['registerDialog']
+      this.$refs.registerDialog.dialogFormVisible = true;
+    }
   }
 };
 </script>
+
 <style lang="less">
 .login-container {
   height: 100%;
@@ -63,86 +129,66 @@ export default {
     rgba(20, 147, 250, 1),
     rgba(1, 198, 250, 1)
   );
-  // 开启弹性布局
+  /* 开启弹性布局 */
   display: flex;
-  //上下居中
+  /* 上下居中 */
   align-items: center;
-  //左右均分
+  /* 左右均分 */
   justify-content: space-around;
+  // 左侧盒子
   .left-box {
     width: 478px;
+    background-color: #f5f5f5;
     height: 550px;
-    background: rgba(245, 245, 245, 1);
-    .ding {
-      width: 100%;
-      height: 50px;
-      img {
-        float: left;
-        width: 30px;
-        margin-left: 54px;
-        margin-top: 40px;
+    // 设置样式 把 表单间隙空出来
+    padding-right: 41px;
+    box-sizing: border-box;
+    .title-box {
+      display: flex;
+      align-items: center;
+      margin-top: 44px;
+      margin-left: 48px;
+      margin-bottom: 27px;
+      .logo {
       }
-      p {
-        float: left;
-        margin: 30px 15px 0 20px;
-        font-size: 30px;
+      .title {
+        font-size: 24px;
+        margin-left: 16px;
+        margin-right: 14px;
       }
-      i {
-        float: left;
+      .line {
         width: 1px;
-        height: 24px;
-        margin-top: 33px;
-        background: rgba(200, 200, 200, 1);
+        height: 28px;
+        background-color: #c7c7c7;
       }
-      span {
-        float: left;
-        margin: 30px 15px 0 15px;
-        font-size: 30px;
+      .sub-title {
+        font-size: 21px;
+        margin-left: 12px;
       }
     }
-    .zhong {
-      // margin-top: 26px;
-      margin-left: 50px;
-      .MobileLei,
-      .cipher {
-        width: 360px;
-        height: 40px;
-        margin-top: 45px;
-      }
-      .code {
-        display: inline-block;
-        width: 240px;
-        height: 40px;
-        margin-top: 45px;
-      }
-      .item {
-        float: right;
-        margin-right: 70px;
-        margin-top: -40px;
-        height: 40px;
-        background-color: #f00;
-      }
+    // 登录验证码
+    .login-code {
+      width: 100%;
+      height: 40.8px;
     }
-    .xieyi {
-      margin-left: 50px;
-      margin-top: 20px;
-      .hezi {
-        width: 18px;
-      }
-      p {
-        display: inline-block;
-        color: rgba(153, 153, 153, 1);
-      }
-      .da {
-        font-size: 16px;
-        margin-top: -5px;
-      }
+    // 底部的按钮
+    .my-btn {
+      width: 100%;
+      margin-top: 26px;
+      margin-left: 0;
     }
-    .buttons{
-      .button_an{
-        margin:20px 0 0 50px; 
-        width: 360px;
-      }
+    // 验证码的 栅格容器
+    .code-col {
+      height: 40.8px;
+    }
+  }
+  // 协议区域的布局
+  .el-checkbox {
+    display: flex;
+    // 上下居中
+    align-items: center;
+    .el-checkbox__label {
+      display: flex;
     }
   }
 }
